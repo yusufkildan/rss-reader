106666c30c7a420b26b69c06b89bf72a004c2253
Yusuf Kıldan <yusufkildan@scorpapp.com>
2019-09-10 21:33:16 +0300

    project initial setup

---
+++ rss-reader/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rss-reader
+//
+//  Created by yusuf_kildan on 10.09.2019.
+//  Copyright © 2019 Yusuf Kıldan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - View's Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+}
+
---
+++ rss-reader/Delegates/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  rss-reader
+//
+//  Created by yusuf_kildan on 10.09.2019.
+//  Copyright © 2019 Yusuf Kıldan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
+
